0329afafba0ffc3866ab5bb065beef4015a05ba4
smhylc <39175076+smhylc@users.noreply.github.com>
2019-10-25 14:46:42 +0300

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 	/**
 	* @author  Semih YOLCU
 	* Created on 2019.10.25
+	*
+	*Problem name : Multiples of 3 and 5 
+	*Problem explanation : If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.
 	*/
 
 public class Problem1 {
 	
 	public static void main(String[] args) {		
-		
+	
 		System.out.println(problem1());
 		
 	}
-
-	// Problem name : Multiples of 3 and 5 
-	// Problem explanation : If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.
-
+	
 	private static int problem1() {
 		int total = 0; 
 
