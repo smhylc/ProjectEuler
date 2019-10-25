469b9bffc0e17ee9488231e10838f99ef3afe3c7
smhylc <39175076+smhylc@users.noreply.github.com>
2019-10-25 14:28:02 +0300

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -22,14 +22,11 @@ public class Problem1 {
 
 		for( int i = 3 ; i<1000 ; i++) {
 			
-			
 		    if (i % 3 == 0 || i % 5 == 0) {
 		    	
 		        total += i;
-		        
 		      }
 		}
-		
 		return total;
 	}
 
