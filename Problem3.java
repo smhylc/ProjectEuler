52cad12542011915e001be64541986a3404add2e
Semih YOLCU <39175076+smhylc@users.noreply.github.com>
2022-06-02 15:43:43 +0300

    Create Problem3.java

---
+++ Problem3.java
@@ -0,0 +1,34 @@
+public final class Problem3 {
+
+	/**
+	 * @author Semih YOLCU
+	 * @create-date 2012-06-02
+	 */
+
+	
+	public static void main(String[] args) {
+		System.out.println(new p003().run());
+	}
+	
+	public String run() {
+		long n = 600851475143L;
+		while (true) {
+			long p = smallestFactor(n);
+			if (p < n)
+				n /= p;
+			else
+				return Long.toString(n);
+		}
+	}
+
+	private static long smallestFactor(long n) {
+		if (n <= 1)
+			throw new IllegalArgumentException();
+		for (long i = 2, end = Library.sqrt(n); i <= end; i++) {
+			if (n % i == 0)
+				return i;
+		}
+		return n;  
+	}
+	
+}
