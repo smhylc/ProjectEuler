6d9e482854159f8b9db6d4cca55766afcc8dd358
Semih YOLCU <39175076+smhylc@users.noreply.github.com>
2022-06-13 17:28:12 +0300

    Update Problem7.java

--- Problem7.java
+++ Problem7.java
@@ -1,10 +1,10 @@
 public final class Problem7 {
 	
 	public static void main(String[] args) {
-		System.out.println(new Problem7().run());
+		System.out.println(new Problem7().calculate());
 	}
 
-	public String run() {
+	public String calculate() {
 		for (int i = 2, count = 0; ; i++) {
 			if (Library.isPrime(i)) {
 				count++;
